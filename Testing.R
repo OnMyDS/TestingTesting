79cad5802be64486000e6cb6e30311b779ae907f
Atticus Solomon <asolomon@lsscm.org>
2024-10-28 09:36:59 -0400

    First one

---
+++ Testing.R
@@ -0,0 +1,22 @@
+#interger
+x <- 2L
+typeof(x)
+
+#double
+y <- 2.5
+typeof(y)
+
+#complex
+z <- 3 + 2i
+typeof(z)
+
+#character
+a <- "h"
+typeof(a)
+
+#logical
+q <- T
+typeof(q)
+
+q2 <- F
+typeof(q2)
